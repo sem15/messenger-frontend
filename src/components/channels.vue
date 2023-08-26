235e8462778f585047276c86cd7c25cf13a97a12
sem15 <maxwellstevene@gmail.com>
2023-08-26 14:33:37 -0400

    initial ui design

---
+++ src/components/channels.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="channels-container">
+        
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'header',
+  }
+  </script>
+  
+
+  <style scoped>
+    .channels-container {
+        width: 100%;
+        height: 100%;
+        /* border-right: 1px solid white; */
+        /* border: 1px solid white; */
+        /* box-shadow: 0.1px 0.2px 1px black; */
+    }
+  </style>
+  
---
+++ src/components/content.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="content-container">
+        
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'header',
+  }
+  </script>
+  
+
+  <style scoped>
+    .content-container {
+        width: 100%;
+        height: 100%;
+        background-color: #262628;
+        /* border: 1px solid white; */
+        box-shadow: 0.2px 3px 1.5px black;
+        z-index: 5;
+    }
+  </style>
+  
